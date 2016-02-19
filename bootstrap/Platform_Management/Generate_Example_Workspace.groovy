6ba270fe1911b8963de5505d7934d34510bc39e9
admin <admin@yahoo.com>
2016-02-19 14:50:19 +0800

    Updated bootstrap

--- bootstrap/Platform_Management/Generate_Example_Workspace.groovy
+++ bootstrap/Platform_Management/Generate_Example_Workspace.groovy
@@ -3,19 +3,19 @@ def platformManagementFolderName= "/Platform_Management"
 def platformManagementFolder = folder(platformManagementFolderName) { displayName('Platform Management') }
 
 // Jobs
-def generateExampleWorkspaceJob = workflowJob(platformManagementFolderName + "/Generate_Example_Workspace")
+def generateExampleWorkspaceJob = workflowJob(platformManagementFolderName + "/Generate_Oracle_Workspace")
  
 generateExampleWorkspaceJob.with{
     parameters{
-        stringParam("projectName","ExampleProject","")
+        stringParam("projectName","HCM","")
         stringParam("projectAdmin","Admin","")
         stringParam("projectDeveloper","Developer","")
         stringParam("projectViewer","Viewer","")
-        stringParam("workspaceName","ExampleWorkspace","")
+        stringParam("workspaceName","Oracle","")
         stringParam("workspaceAdmin","Admin","")
         stringParam("workspaceDeveloper","Developer","")
         stringParam("workspaceViewer","Viewer","")
-        stringParam("cartridgeURL","ssh://jenkins@gerrit:29418/cartridges/adop-cartridge-java.git","")
+        stringParam("cartridgeURL","ssh://jenkins@gerrit:29418/cartridges/adop-cartridge-hcm.git","")
     }
     properties {
         rebuild {
