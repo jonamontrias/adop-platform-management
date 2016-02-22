95ea13508604e185576ea5d8091505ca0ccbe5ee
admin <admin@yahoo.com>
2016-02-22 13:04:20 +0800

    updated!

--- bootstrap/Platform_Management/Generate_Example_Workspace.groovy
+++ bootstrap/Platform_Management/Generate_Example_Workspace.groovy
@@ -3,15 +3,15 @@ def platformManagementFolderName= "/Platform_Management"
 def platformManagementFolder = folder(platformManagementFolderName) { displayName('Platform Management') }
 
 // Jobs
-def generateExampleWorkspaceJob = workflowJob(platformManagementFolderName + "/Generate_Oracle_Workspace")
+def generateExampleWorkspaceJob = workflowJob(platformManagementFolderName + "/Generate_Example_Workspace")
  
 generateExampleWorkspaceJob.with{
     parameters{
-        stringParam("projectName","HCM","")
+        stringParam("projectName","ExampleProject","")
         stringParam("projectAdmin","Admin","")
         stringParam("projectDeveloper","Developer","")
         stringParam("projectViewer","Viewer","")
-        stringParam("workspaceName","Oracle","")
+        stringParam("workspaceName","ExampleWorkspace","")
         stringParam("workspaceAdmin","Admin","")
         stringParam("workspaceDeveloper","Developer","")
         stringParam("workspaceViewer","Viewer","")
